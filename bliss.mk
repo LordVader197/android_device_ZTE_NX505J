d2943ebbeeeb52d8e919f10f54a2527191be3d92
Demelza <demeruza@yahoo.fr>
2015-08-04 14:37:17 +0200

    Blissify device tree

---
+++ bliss.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/ZTE/NX505J/full_NX505J.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+
+PRODUCT_RELEASE_NAME := bliss_NX505J
+PRODUCT_NAME := bliss_NX505J
+
+
+
