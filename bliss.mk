b461d1a1fcc06d03709da025077e150d5fea7df8
Demelza <demeruza@yahoo.fr>
2015-08-12 11:09:52 +0200

    Update to Rel. V5

--- bliss.mk
+++ bliss.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/ZTE/NX505J/full_NX505J.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Bliss stuff.
 $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := bliss_NX505J
