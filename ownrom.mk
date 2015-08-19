3416780d4463a24390ca820d0570220dbf728394
Demelza <demeruza@yahoo.fr>
2015-08-19 15:28:36 +0200

    Update to Rel. V7

--- ownrom.mk
+++ ownrom.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/ZTE/NX505J/full_NX505J.mk)
 
-# Inherit some common OwnRom stuff.
+# Inherit some common OwnROM stuff.
 $(call inherit-product, vendor/ownrom/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := ownrom_NX505J
