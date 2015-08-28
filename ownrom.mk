e1a621cca69e3b2f15f55db85d59bd30b10e97a2
LordVader197 <LordVader197@users.noreply.github.com>
2015-08-28 20:52:34 +0500

    Fixed Caps abuse
    I'm a bit sensitive when caps lock is hit unnecessarily

--- ownrom.mk
+++ ownrom.mk
@@ -3,8 +3,8 @@ $(call inherit-product, device/ZTE/NX505J/full_NX505J.mk)
 # Inherit some common OwnROM stuff.
 $(call inherit-product, vendor/ownrom/config/common_full_phone.mk)
 
-PRODUCT_RELEASE_NAME := ownrom_NX505J
-PRODUCT_NAME := ownrom_NX505J
+PRODUCT_RELEASE_NAME := ownrom_nx505j
+PRODUCT_NAME := ownrom_nx505j
 
 
 
